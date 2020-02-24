fcabfb5aaa329fa3fe1771a930e852fd53f2ce81
Abbasi, Khawar <khawar.abbasi@intel.com>
2020-02-23 22:14:12 -0800

    Copyright year update

--- examples/python/monitoring.py
+++ examples/python/monitoring.py
@@ -1,7 +1,7 @@
 ################################################################################
 # BSD LICENSE
 #
-# Copyright(c) 2019 Intel Corporation. All rights reserved.
+# Copyright(c) 2019-2020 Intel Corporation. All rights reserved.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
--- tools/membw/membw.c
+++ tools/membw/membw.c
@@ -1,7 +1,7 @@
 /*
  * BSD LICENSE
  *
- * Copyright(c) 2018-2019 Intel Corporation. All rights reserved.
+ * Copyright(c) 2018-2020 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
