a78de55beb66738550cfb436e9c3cc5cfc533a3d
Michał Aleksiński <michalx.aleksinski@intel.com>
2021-09-06 10:52:47 +0000

    lib: Added setup.cfg

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -1,25 +1,4 @@
 from setuptools import setup
 
-with open('README.txt', 'r') as readme_file:
-    long_description = readme_file.read()
-
-setup(
-    name='pqos',
-    version='4.2.0',
-    author='Khawar Abbasi',
-    author_email='khawar.abbasi@intel.com',
-    description='Python interface for PQoS library',
-    long_description=long_description,
-    long_description_content_type='text/plain',
-    url='https://github.com/intel/intel-cmt-cat',
-    project_urls={
-        'Bug Tracker': 'https://github.com/intel/intel-cmt-cat/issues'
-    },
-    license='BSD 3-Clause License',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License'
-    ],
-    packages=['pqos', 'pqos.test'],
-    python_requires='>=3.5'
-)
+if __name__ == '__main__':
+    setup()
