a11b3838c51a8cc6175ccca34041c9b4beb09d6f
Shanahan, Corey <corey.shanahan@intel.com>
2023-02-10 10:34:40 +0000

    appqos_client [pool_config_component] Added beforeEach + unit test for getPool

--- appqos_client/src/app/components/rap-config/pool-config/pool-config.component.spec.ts
+++ appqos_client/src/app/components/rap-config/pool-config/pool-config.component.spec.ts
@@ -26,3 +26,80 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+import { MockBuilder, MockInstance, MockRender, ngMocks } from 'ng-mocks';
+import { PoolConfigComponent } from './pool-config.component';
+import { CacheAllocation } from '../../system-caps/system-caps.model';
+import { Pools, Apps } from '../../overview/overview.model';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { AppqosService } from 'src/app/services/appqos.service';
+import { BehaviorSubject, EMPTY, of } from 'rxjs';
+import { LocalService } from 'src/app/services/local.service';
+
+describe('Given poolConfigComponent', () => {
+  beforeEach(() => {
+    return MockBuilder(PoolConfigComponent)
+      .mock(SharedModule)
+      .mock(AppqosService, {
+        getPools: () => EMPTY
+      })
+      .mock(LocalService, {
+        getL3CatEvent: () => EMPTY,
+        getL2CatEvent: () => EMPTY
+      })
+  })
+
+  const mockedPools: Pools[] = [
+    { id: 0, name: 'pool_0', cores: [1, 2, 3] },
+    { id: 1, name: 'pool_1', cores: [4, 5, 6] },
+    { id: 2, name: 'pool_2', cores: [7, 8, 9] },
+    { id: 3, name: 'pool_3', cores: [10, 11, 12] }
+  ];
+
+  const mockedCache: CacheAllocation = {
+    cache_size: 44040192,
+    cdp_enabled: false,
+    cdp_supported: false,
+    clos_num: 15,
+    cw_num: 12,
+    cw_size: 3670016
+  }
+
+  const params = {
+    pools: mockedPools,
+    apps: []
+  }
+
+  MockInstance.scope('case');
+
+  describe('when getPool method is called', () => {
+    it('it should update selected pool', () => {
+      const poolID = 0;
+      const mockedL3Bitmask = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+      const mockedL2BitMask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+
+      const mockedPools: Pools[] = [
+        { id: 0, name: 'pool_0', cores: [1, 2, 3], l3cbm: 127, l2cbm: 7 },
+      ];
+
+      const params = {
+        pools: mockedPools,
+        apps: []
+      }
+
+      const catEvent = new BehaviorSubject<CacheAllocation>(mockedCache);
+
+      MockInstance(LocalService, 'getL3CatEvent', () => catEvent);
+      MockInstance(LocalService, 'getL2CatEvent', () => catEvent);
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolConfigComponent, params);
+
+      component.getPool(poolID);
+      expect(component.selected).toBe(mockedPools[poolID].name);
+      expect(component.pool.l3Bitmask).toEqual(mockedL3Bitmask);
+      expect(component.pool.l2Bitmask).toEqual(mockedL2BitMask);
+    })
+  })
+});
