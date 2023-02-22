ada2979a9eb66f3fc8f79e10bf18572ef13ad753
Shanahan, Corey <corey.shanahan@intel.com>
2023-02-22 14:59:46 +0000

    appqos_client: Added pool-add-dialog unit tests

--- appqos_client/src/app/components/rap-config/pool-config/pool-add-dialog/pool-add-dialog.component.spec.ts
+++ appqos_client/src/app/components/rap-config/pool-config/pool-add-dialog/pool-add-dialog.component.spec.ts
@@ -26,3 +26,381 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+import { MockBuilder, MockInstance, MockRender, ngMocks } from "ng-mocks"
+import { EMPTY, of, throwError } from "rxjs";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { AppqosService } from "src/app/services/appqos.service";
+import { LocalService } from "src/app/services/local.service";
+import { SharedModule } from "src/app/shared/shared.module";
+import { SnackBarService } from "src/app/shared/snack-bar.service";
+import { PoolAddDialogComponent } from "./pool-add-dialog.component";
+
+describe('Given poolAddDialogComponent', () => {
+  beforeEach(() => {
+    return MockBuilder(PoolAddDialogComponent)
+      .mock(SharedModule)
+      .mock(LocalService, {
+        getCapsEvent: () => EMPTY,
+        getMbaCtrlEvent: () => EMPTY,
+      })
+      .mock(AppqosService, {
+        postPool: () => EMPTY
+      })
+      .mock(SnackBarService)
+      .mock(MatDialogRef)
+  })
+
+  const mockedCaps = [
+    'l3cat',
+    'l2cat',
+    'mba'
+  ]
+
+  const mockedMbaCtrl = false;
+  const mockedData = {
+    l2cwNum: 10,
+    l3cwNum: 12
+  }
+
+  MockInstance.scope('case');
+
+  describe('when initialized', () => {
+    it('it should populate the caps array & set mbaCtrl', () => {
+      const getCapsEventSpy = jasmine.createSpy().and.returnValue(of(mockedCaps));
+      const getMbaCtrlEventSpy = jasmine.createSpy().and.returnValue(of(mockedMbaCtrl));
+
+      MockInstance(LocalService, 'getCapsEvent', getCapsEventSpy);
+      MockInstance(LocalService, 'getMbaCtrlEvent', getMbaCtrlEventSpy);
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+
+      expect(getCapsEventSpy).toHaveBeenCalledTimes(1);
+      expect(component.caps).toEqual(mockedCaps);
+
+      expect(getMbaCtrlEventSpy).toHaveBeenCalledTimes(1);
+      expect(component.mbaCtrl).toBe(mockedMbaCtrl);
+    })
+  })
+
+  describe('when savePool method is called', () => {
+    MockInstance(LocalService, 'getCapsEvent', () => of(mockedCaps));
+    MockInstance(LocalService, 'getMbaCtrlEvent', () => of(mockedMbaCtrl));
+
+    it('it should create a pool', () => {
+      const name = 'pool_0';
+      const cores = '0-11';
+      const mockedCoresList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+      const mockedPool = {
+        name: name,
+        cores: mockedCoresList,
+        l3cbm: (1 << mockedData.l3cwNum) - 1,
+        l2cbm: (1 << mockedData.l2cwNum) - 1,
+        mba: 100
+      }
+
+      const getCoresDashSpy = spyOn(LocalService.prototype, 'getCoresDash').and.callThrough();
+      MockInstance(LocalService, 'getCoresDash', getCoresDashSpy);
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+
+      const postPoolSpy = spyOn(component, 'postPool');
+
+      component.form.patchValue({ name, cores });
+
+      const savePoolButton = ngMocks.find('#save-pool-button');
+      savePoolButton.triggerEventHandler('click', null);
+
+      expect(component.form.valid).toBeTrue();
+      expect(getCoresDashSpy).toHaveBeenCalledWith(cores);
+      expect(component.coresList).toEqual(mockedCoresList);
+      expect(postPoolSpy).toHaveBeenCalledWith(mockedPool);
+    })
+
+    it('it should return if the form is not valid', () => {
+      const name = '';
+      const cores = '';
+      const coresErrorMessage = 'Cores is required!';
+      const nameErrorMessage = 'Name is required!';
+
+      const getCoresDashSpy = jasmine.createSpy();
+      MockInstance(LocalService, 'getCoresDash', getCoresDashSpy);
+
+      const fixture = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      const postPoolSpy = spyOn(component, 'postPool');
+
+      component.form.patchValue({ name, cores });
+      fixture.detectChanges();
+
+      const coresReqError = ngMocks.formatText(
+        ngMocks.find('#cores-req-error')
+      );
+
+      const nameReqError = ngMocks.formatText(
+        ngMocks.find('#name-req-error')
+      );
+
+      const savePoolButton = ngMocks.find('#save-pool-button');
+      savePoolButton.triggerEventHandler('click', null);
+
+      expect(coresReqError).toBe(coresErrorMessage);
+      expect(component.form.controls['cores'].errors?.['required']).toBeTrue();
+
+      expect(nameReqError).toBe(nameErrorMessage);
+      expect(component.form.controls['name'].errors?.['required']).toBeTrue();
+
+      expect(getCoresDashSpy).not.toHaveBeenCalled();
+      expect(postPoolSpy).not.toHaveBeenCalled();
+    })
+
+    it('it should throw error if the name input field exceeds 80 characters', () => {
+      const cores = '11,22-43';
+      const coresErrorMessage = 'max length 80 characters';
+
+      let name = 'p';
+      for (let i = 0; i < 80; i++){
+        name += 'p';
+      }
+      
+      const fixture = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ name, cores });
+      fixture.detectChanges();
+
+      const coresError = ngMocks.formatText(
+        ngMocks.find('#max-name-error')
+      )
+
+      expect(coresError).toBe(coresErrorMessage);
+      expect(component.form.controls['name'].hasError('maxlength')).toBeTrue();
+    })
+
+    it('it should throw error if cores exceeds MAX_CORES',  () => {
+      const name = 'pool_0';
+      const cores = '1024,1025';
+      const maxCoresErrorText = 'limit cores to maximum number of 1024'
+
+      const fixture = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      const postPoolSpy = spyOn(component, 'postPool');
+
+      component.form.patchValue({ name, cores });
+      
+      const savePoolButton = ngMocks.find('#save-pool-button');
+      savePoolButton.triggerEventHandler('click', null);
+      
+      fixture.detectChanges();
+      
+      const maxCoresError = ngMocks.formatText(
+        ngMocks.find('#max-cores-error')
+      );
+      
+      expect(maxCoresError).toBe(maxCoresErrorText);
+      expect(postPoolSpy).not.toHaveBeenCalled();
+      expect(component.form.invalid).toBeTrue();
+    })
+
+    it('it should throw error if cores input field exceeds 4096 characters', () => {
+      const name = 'pool_0';
+      const maxCoresErrorText = 'max length 4096 characters';
+      
+      let cores: string = '0';
+      for (let i = 1; i < 5000; i++){
+        cores = `${cores}, ${i}`;
+      }
+      
+      const fixture = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      const postPoolSpy = spyOn(component, 'postPool');
+
+      component.form.patchValue({ name, cores });
+      fixture.detectChanges();
+
+      const maxCoresError = ngMocks.formatText(
+        ngMocks.find('#max-char-cores-error')
+      );
+      
+      expect(maxCoresError).toBe(maxCoresErrorText);
+      expect(postPoolSpy).not.toHaveBeenCalled();
+      expect(component.form.invalid).toBeTrue();
+      expect(component.form.controls['cores'].hasError('maxlength')).toBeTrue();
+    })
+
+    it('it should throw error if cores are not formatted correctly', () => {
+      const name = 'pool_0';
+      const cores = '1,11-14,45-75';
+      const coresErrorMessage = 'List of cores e.g. 1,2 or 1,2-5 or 1-5';
+
+      const fixture = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.form.patchValue({ name, cores });
+      fixture.detectChanges();
+
+      const coresError = ngMocks.formatText(
+        ngMocks.find('#cores-pattern-error')
+      );
+
+      expect(coresError).toBe(coresErrorMessage);
+      expect(component.form.controls['cores'].hasError('pattern')).toBeTrue();
+    })
+ 
+    it('it should create a pool', () => {
+      const name = 'pool_0';
+      const cores = '11,12';
+      const mockedCoresList = [11, 12];
+      const mockedPool = {
+        name: name,
+        cores: mockedCoresList,
+        l3cbm: (1 << mockedData.l3cwNum) - 1,
+        l2cbm: (1 << mockedData.l2cwNum) - 1,
+        mba_bw: Math.pow(2, 32) - 1
+      }
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+
+      const postPoolSpy = spyOn(component, 'postPool');
+
+      component.mbaCtrl = true;
+      component.form.patchValue({ name, cores });
+
+      const savePoolButton = ngMocks.find('#save-pool-button');
+      savePoolButton.triggerEventHandler('click', null);
+
+      expect(postPoolSpy).toHaveBeenCalledWith(mockedPool);
+      expect(component.form.valid).toBeTrue();
+    })
+  });
+
+  describe('when postPool method is called', () => {
+    const mockedPool = {
+      name: 'pool_0',
+      cores: [1, 2, 3, 4, 5, 6, 7],
+      mba: 100,
+      l3cbm: 12,
+      l2cbm: 10
+    }
+
+    MockInstance(LocalService, 'getCapsEvent', () => of(mockedCaps));
+    MockInstance(LocalService, 'getMbaCtrlEvent', () => of(mockedMbaCtrl));
+
+    it('it should display response', () => {
+      const mockedResponse = {
+        status: 200,
+        message: 'NEW POOL 0 added'
+      }
+
+      const postPoolSpy = jasmine.createSpy().and.returnValue(of(mockedResponse));
+      const displayInfoSpy = jasmine.createSpy();
+      const closeSpy = jasmine.createSpy();
+
+      MockInstance(AppqosService, 'postPool', postPoolSpy);
+      MockInstance(SnackBarService, 'displayInfo', displayInfoSpy);
+      MockInstance(MatDialogRef, 'close', closeSpy);
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolAddDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedData
+          }
+        ]
+      });
+
+      component.postPool(mockedPool);
+
+      expect(postPoolSpy).toHaveBeenCalledOnceWith(mockedPool);
+      expect(displayInfoSpy).toHaveBeenCalledOnceWith(mockedResponse.message);
+      expect(closeSpy).toHaveBeenCalledOnceWith(mockedResponse);
+    })
+
+    it('it should handle error', () => {
+      const mockedError: Error = {
+        name: 'error',
+        message: 'poolPut error'
+      }
+
+      const postPoolSpy = jasmine.createSpy().and.returnValue(
+        throwError(() => mockedError)
+      );
+      const handleErrorSpy = jasmine.createSpy();
+
+      MockInstance(AppqosService, 'postPool', postPoolSpy);
+      MockInstance(SnackBarService, 'handleError', handleErrorSpy);
+
+      const {
+        point: { componentInstance: component }
+      } = MockRender(PoolAddDialogComponent);
+
+      component.postPool(mockedPool);
+
+      expect(postPoolSpy).toHaveBeenCalledOnceWith(mockedPool);
+      expect(handleErrorSpy).toHaveBeenCalledOnceWith(mockedError.message);
+    })
+  })
+});
