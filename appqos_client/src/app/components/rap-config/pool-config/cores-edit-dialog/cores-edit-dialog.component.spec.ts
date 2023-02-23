5936409e047bd5d39ef5827baef4f621b6104ee2
Shanahan, Corey <corey.shanahan@intel.com>
2023-02-23 15:13:44 +0000

    appqos_client: Added cores-edit-dialog unit tests

--- appqos_client/src/app/components/rap-config/pool-config/cores-edit-dialog/cores-edit-dialog.component.spec.ts
+++ appqos_client/src/app/components/rap-config/pool-config/cores-edit-dialog/cores-edit-dialog.component.spec.ts
@@ -26,3 +26,235 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog"
+import { MockBuilder, MockInstance, MockRender, ngMocks } from "ng-mocks"
+import { AppqosService } from "src/app/services/appqos.service"
+import { LocalService } from "src/app/services/local.service"
+import { SharedModule } from "src/app/shared/shared.module"
+import { SnackBarService } from "src/app/shared/snack-bar.service"
+import { CoresEditDialogComponent } from "./cores-edit-dialog.component"
+import { EMPTY, of, throwError } from 'rxjs';
+import { Pools } from "src/app/components/overview/overview.model"
+import { MatError } from "@angular/material/form-field"
+
+describe('Given coresEditDialog component', () => {
+  beforeEach(() => {
+    return MockBuilder(CoresEditDialogComponent)
+      .mock(SharedModule)
+      .mock(AppqosService, {
+        poolPut: () => EMPTY
+      })
+      .mock(LocalService)
+      .mock(SnackBarService)
+      .mock(MatDialogRef)
+  })
+
+  const mockedPool: Pools = {
+    id: 0,
+    name: 'pool_0',
+    cores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  }
+
+  MockInstance.scope('case');
+
+  describe('when saveCores method is called', () => {
+    it('it should return if the form is invalid', () => {
+      const invalidCores = '';
+      const coresError = 'Cores is required!';
+
+      const poolPutSpy = jasmine.createSpy();
+      MockInstance(AppqosService, 'poolPut', poolPutSpy);
+
+      const fixture = MockRender(CoresEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedPool
+          }
+        ]
+      });
+
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ cores: invalidCores });
+      fixture.detectChanges();
+
+      const expectCoresError = ngMocks.formatText(
+        ngMocks.findAll(MatError)[0]
+      );
+
+      const saveCoresButton = ngMocks.find('#save-cores-button');
+      saveCoresButton.triggerEventHandler('click', null);
+
+      expect(expectCoresError).toBe(coresError);
+      expect(poolPutSpy).not.toHaveBeenCalled();
+      expect(component.form.invalid).toBeTrue();
+    })
+
+    it('it should return if one of the cores exceed MAX_CORES', () => {
+      const mockedCores = '1024,1025';
+      const coresError = 'limit cores to maximum number of 1024';
+
+      const poolPutSpy = jasmine.createSpy();
+      MockInstance(AppqosService, 'poolPut', poolPutSpy);
+
+      const fixture = MockRender(CoresEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedPool
+          }
+        ]
+      });
+
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ cores: mockedCores });
+
+      const saveCoresButton = ngMocks.find('#save-cores-button');
+      saveCoresButton.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+
+      const expectCoresError = ngMocks.formatText(
+        ngMocks.findAll(MatError)[0]
+      );
+
+      expect(expectCoresError).toBe(coresError);
+      expect(component.form.controls['cores'].hasError('incorrect')).toBeTrue();
+      expect(poolPutSpy).not.toHaveBeenCalled();
+    })
+
+    it('it should throw error if cores are not formatted correctly', () => {
+      const mockedCores = '25,35-42,50-67';
+      const coresError = 'List of cores e.g. 1,2 or 1,2-5 or 1-5';
+
+      const poolPutSpy = jasmine.createSpy();
+      MockInstance(AppqosService, 'poolPut', poolPutSpy);
+
+      const fixture = MockRender(CoresEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedPool
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.form.patchValue({ cores: mockedCores });
+      fixture.detectChanges();
+
+      const expectCoresError = ngMocks.formatText(
+        ngMocks.findAll(MatError)[0]
+      )
+
+      expect(expectCoresError).toBe(coresError);
+      expect(component.form.controls['cores'].hasError('pattern')).toBeTrue();
+    })
+
+    it('it should throw error if the input field exceeds 4096 characters', () => {
+      const coresError = 'max length 4096 characters';
+      let cores: string = '0';
+      for (let i = 1; i < 5000; i++){
+        cores = `${cores}, ${i}`;
+      }
+
+      const fixture = MockRender(CoresEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: mockedPool
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.form.patchValue({ cores });
+      fixture.detectChanges();
+
+      const expectCoresError = ngMocks.formatText(
+        ngMocks.findAll(MatError)[1]
+      )
+
+      expect(expectCoresError).toBe(coresError);
+      expect(component.form.controls['cores'].hasError('maxlength'));
+    })
+
+    it('it should handle PUT request response and close the dialog', () => {
+      const poolId = 0;
+      const mockedCores = '22-30';
+      const mockedCoresList = [22, 23, 24, 25, 26, 27, 28, 29, 30];
+      const mockedResponse = {
+        message: `POOL ${poolId} updated`
+      }
+
+      const getCoresDashSpy = jasmine.createSpy().and.returnValue(mockedCoresList);
+      const poolPutSpy = jasmine.createSpy().and.returnValue(of(mockedResponse));
+      const snackBarSpy = jasmine.createSpy();
+      const closeSpy = jasmine.createSpy();
+
+      MockInstance(LocalService, 'getCoresDash', getCoresDashSpy);
+      MockInstance(AppqosService, 'poolPut', poolPutSpy);
+      MockInstance(SnackBarService, 'displayInfo', snackBarSpy);
+      MockInstance(MatDialogRef, 'close', closeSpy);
+
+      const fixture = MockRender(CoresEditDialogComponent,
+        {},
+        {
+          providers: [
+            { provide: MAT_DIALOG_DATA, useValue: mockedPool }
+          ]
+        }
+      )
+
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ cores: mockedCores });
+
+      const saveCoresButton = ngMocks.find('#save-cores-button');
+      saveCoresButton.triggerEventHandler('click', null);
+
+      expect(getCoresDashSpy).toHaveBeenCalledOnceWith(mockedCores);
+      expect(poolPutSpy).toHaveBeenCalledOnceWith({ cores: mockedCoresList }, poolId);
+      expect(snackBarSpy).toHaveBeenCalledOnceWith(mockedResponse.message);
+      expect(closeSpy).toHaveBeenCalledOnceWith(true);
+    })
+
+    it('it should handle PUT request error', () => {
+      const poolId = 0;
+      const mockedCores = '15-21';
+      const mockedCoresList = [15, 16, 17, 18, 19, 20, 21];
+      const mockedError = {
+        message: `POOL ${poolId} not updated`
+      }
+
+      const getCoresDashSpy = jasmine.createSpy().and.returnValue(mockedCoresList);
+      const poolPutSpy = jasmine.createSpy().and.returnValue(
+        throwError(() => mockedError)
+      );
+      const snackBarSpy = jasmine.createSpy();
+
+      MockInstance(LocalService, 'getCoresDash', getCoresDashSpy);
+      MockInstance(AppqosService, 'poolPut', poolPutSpy);
+      MockInstance(SnackBarService, 'handleError', snackBarSpy);
+
+      const fixture = MockRender(CoresEditDialogComponent,
+        {},
+        {
+          providers: [
+            { provide: MAT_DIALOG_DATA, useValue: mockedPool }
+          ]
+        }
+      )
+
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ cores: mockedCores });
+
+      const saveCoresButton = ngMocks.find('#save-cores-button');
+      saveCoresButton.triggerEventHandler('click', null);
+
+      expect(getCoresDashSpy).toHaveBeenCalledOnceWith(mockedCores);
+      expect(poolPutSpy).toHaveBeenCalledOnceWith({ cores: mockedCoresList }, poolId);
+      expect(snackBarSpy).toHaveBeenCalledOnceWith(mockedError.message);
+    })
+  })
+})
