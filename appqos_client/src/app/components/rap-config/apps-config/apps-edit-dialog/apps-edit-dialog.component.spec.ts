e5f3eb049d1398b2ecae5a3f3180e62a6dc32138
Shanahan, Corey <corey.shanahan@intel.com>
2023-03-08 10:47:05 +0000

    appqos_client: Added apps-edit-dialog unit tests

--- appqos_client/src/app/components/rap-config/apps-config/apps-edit-dialog/apps-edit-dialog.component.spec.ts
+++ appqos_client/src/app/components/rap-config/apps-config/apps-edit-dialog/apps-edit-dialog.component.spec.ts
@@ -26,3 +26,555 @@ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog"
+import { MatError } from "@angular/material/form-field"
+import { MockBuilder, MockInstance, MockRender, ngMocks } from "ng-mocks"
+import { Apps, Pools } from "src/app/components/overview/overview.model"
+import { resMessage } from "src/app/components/system-caps/system-caps.model"
+import { AppqosService } from "src/app/services/appqos.service"
+import { LocalService } from "src/app/services/local.service"
+import { SharedModule } from "src/app/shared/shared.module"
+import { SnackBarService } from "src/app/shared/snack-bar.service"
+import { AppsEditDialogComponent } from "./apps-edit-dialog.component"
+import { of, throwError } from "rxjs"
+import { MatSelect, MatSelectChange } from "@angular/material/select"
+
+describe('Given AppsEditDialog component', () => {
+  MockInstance.scope('case');
+  
+  beforeEach(() => {
+    getCoresDashSpy = spyOn(LocalService.prototype, 'getCoresDash').and.callThrough();
+    getPidsDashSpy = spyOn(LocalService.prototype, 'getPidsDash').and.callThrough();
+
+    MockInstance(LocalService, 'getCoresDash', getCoresDashSpy);
+    MockInstance(LocalService, 'getPidsDash', getPidsDashSpy);
+
+    return MockBuilder(AppsEditDialogComponent)
+      .mock(SharedModule)
+      .mock(LocalService)
+      .mock(AppqosService)
+      .mock(SnackBarService)
+      .mock(MatDialogRef)
+  })
+
+  let getPidsDashSpy: any,
+    getCoresDashSpy: any;
+
+  type PostApp = Omit<Apps, 'id'>;
+
+  const mockedPools: Pools[] = [
+    { id: 0, name: 'pool_0', cores: [1, 2, 3] },
+    { id: 1, name: 'pool_1', cores: [4, 5, 6] },
+    { id: 2, name: 'pool_2', cores: [7, 8, 9] }
+  ]
+
+  const mockApp: Apps = {
+    id: 0,
+    name: 'app_0',
+    pids: [4156985, 4156986, 4156987],
+    cores: mockedPools[0].cores,
+    pool_id: 0
+  }
+
+  describe('when initialise', () => {
+    it('it should populate input fields', () => {
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      expect(component.form.controls['name'].value).toBe(mockApp.name);
+      expect(component.form.controls['pids'].value).toBe(mockApp.pids.toString());
+      expect(component.form.controls['cores'].value).toBe(mockedPools[0].cores.toString());
+      expect(component.form.controls['pool'].value).toBe(mockApp.pool_id);
+    })
+
+    it('it should populate the correct cores', () => {
+      const mockedApp: Apps = {
+        id: 0,
+        name: 'app_0',
+        pids: [4156985, 4156986, 4156987],
+        cores: [2],
+        pool_id: 0
+      }
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockedApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      expect(component.form.controls['cores'].value)
+        .toBe(mockedApp.cores?.toString());
+    })
+
+    it('it should popluate the correct cores if undefined', () => {
+      const mockedApp: Apps = {
+        id: 0,
+        name: 'app_0',
+        pids: [4156985, 4156986, 4156987],
+        pool_id: 0
+      }
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockedApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      expect(component.form.controls['cores'].value)
+        .toBe(mockedPools[0].cores.toString());
+    })
+  })
+
+  describe('when form is invalid ', () => {
+    it('it should display required error', () => {
+      const name = '';
+      const pids = '';
+      const nameError = 'name is required';
+      const pidsError = 'pids is required!';
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.form.patchValue({ name, pids });
+      fixture.detectChanges();
+
+      const errors = ngMocks.findAll(MatError);
+
+      const errorMessages = ngMocks.formatText(errors);
+
+      expect(errorMessages.includes(nameError)).toBeTrue();
+      expect(errorMessages.includes(pidsError)).toBeTrue();
+    })
+
+    it('it should display max length error', () => {
+      let name = 'P';
+      for (let i = 0; i < 90; i++) {
+        name = `${name}p`;
+      }
+
+      let cores = '1';
+      for (let i = 0; i < 4096; i++) {
+        cores += `1`;
+      }
+
+      const nameError = 'max length 80 characters';
+      const pidsError = 'max length 4096 characters';
+      const coresError = 'max length 4096 characters';
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.form.patchValue({ name, pids: cores, cores });
+      fixture.detectChanges();
+
+      const errors = ngMocks.findAll(MatError);
+
+      const errorMessages = ngMocks.formatText(errors);
+
+      expect(errorMessages.includes(nameError)).toBeTrue();
+      expect(errorMessages.includes(pidsError)).toBeTrue();
+      expect(errorMessages.includes(coresError)).toBeTrue();
+    })
+  })
+
+  it('it should display the pattern error', () => {
+    const pids = '4156985-4156991, 4156992-4156998';
+    const cores = '1,3,4-7,8-11';
+
+    const pidsError = 'List of PID’s e.g. 2651, 2665 or 6490-6510';
+    const coresError = 'List of CORE’s e.g. 1,2 or 1,2-5 or 1-5';
+
+    const fixture = MockRender(AppsEditDialogComponent, {}, {
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { pools: mockedPools, app: mockApp }
+        }
+      ]
+    });
+    const component = fixture.point.componentInstance;
+
+    component.form.patchValue({ cores, pids });
+    fixture.detectChanges();
+
+    const errors = ngMocks.findAll(MatError);
+
+    const errorMessages = ngMocks.formatText(errors);
+
+    expect(errorMessages.includes(pidsError)).toBeTrue();
+    expect(errorMessages.includes(coresError)).toBeTrue();
+  })
+
+  describe('when saveApp method is called', () => {
+    it('it should return if the form is invalid', () => {
+      const name = '';
+      const pids = '';
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      const updateAppSpy = spyOn(component, 'updateApp');
+
+      component.form.patchValue({ name, pids });
+      fixture.detectChanges();
+
+      const updateButton = ngMocks.find('#update-button');
+      updateButton.triggerEventHandler('click', null);
+
+      expect(updateAppSpy).not.toHaveBeenCalled();
+    })
+
+    it('it should save the app using app cores when specified', () => {
+      const name = 'app';
+      const pids = '41539-41545';
+      const cores = '2';
+      const pidsList = [41539, 41540, 41541, 41542, 41543, 41544, 41545];
+      const coresList = [2];
+      const poolID = 0;
+
+      const mockedPostApp: PostApp = {
+        name: name,
+        pids: pidsList,
+        cores: coresList,
+        pool_id: poolID
+      }
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ name, pids, cores });
+
+      const updateAppSpy = spyOn(component, 'updateApp');
+
+      const updateButton = ngMocks.find('#update-button');
+      updateButton.triggerEventHandler('click', null);
+
+      expect(updateAppSpy).toHaveBeenCalledWith(mockedPostApp);
+    })
+
+    it('it should save the app using pool cores when not specified', () => {
+      const name = 'app';
+      const pids = '41539,41545';
+      const cores = '';
+      const pidsList = [41539, 41545];
+      const poolID = 0;
+
+      const mockedPostApp: PostApp = {
+        name: name,
+        pids: pidsList,
+        cores: mockedPools[poolID].cores,
+        pool_id: poolID
+      }
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      component.form.patchValue({ name, pids, cores });
+
+
+      const updateAppSpy = spyOn(component, 'updateApp');
+
+      const updateButton = ngMocks.find('#update-button');
+      updateButton.triggerEventHandler('click', null);
+
+      expect(updateAppSpy).toHaveBeenCalledWith(mockedPostApp);
+    })
+  })
+
+  describe('when updateApp method is called', () => {
+    const appId = 0;
+    const mockedPostApp: PostApp = {
+      name: 'app',
+      pids: [45836, 45837, 45838, 45839, 45840],
+      pool_id: 0,
+      cores: [1, 2, 3]
+    }
+
+    it('it should handle response', () => {
+      const mockedResponse: resMessage = {
+        message: `APP ${appId} updated`
+      }
+
+      const appPutSpy = jasmine.createSpy().and.returnValue(of(mockedResponse));
+      const displayInfoSpy = jasmine.createSpy();
+      const closeSpy = jasmine.createSpy();
+
+      MockInstance(AppqosService, 'appPut', appPutSpy);
+      MockInstance(SnackBarService, 'displayInfo', displayInfoSpy);
+      MockInstance(MatDialogRef, 'close', closeSpy);
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      component.updateApp(mockedPostApp);
+
+      expect(appPutSpy).toHaveBeenCalledOnceWith(mockedPostApp, appId);
+      expect(displayInfoSpy).toHaveBeenCalledOnceWith(mockedResponse.message);
+      expect(closeSpy).toHaveBeenCalledTimes(1);
+    })
+
+    it('it should handle error', () => {
+      const mockedError: Error = {
+        name: 'error',
+        message: `APP ${appId} not updated`
+      }
+
+      const appPutSpy = jasmine.createSpy().and.returnValue(
+        throwError(() => mockedError)
+      );;
+      const handleErrorSpy = jasmine.createSpy();
+
+      MockInstance(AppqosService, 'appPut', appPutSpy);
+      MockInstance(SnackBarService, 'handleError', handleErrorSpy);
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+      component.updateApp(mockedPostApp);
+
+      expect(appPutSpy).toHaveBeenCalledOnceWith(mockedPostApp, appId);
+      expect(handleErrorSpy).toHaveBeenCalledOnceWith(mockedError.message);
+    })
+  })
+
+  describe('when getPids method is called', () => {
+    it('it should call getPidsDash', () => {
+      const pids = '36951-36959';
+      const pidsList = [
+        36951, 36952, 36953, 36954, 36955, 36956, 36957, 36958, 36959
+      ];
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.getPids(pids);
+      expect(getPidsDashSpy).toHaveBeenCalledOnceWith(pids);
+      expect(component.pidsList).toEqual(pidsList);
+    })
+
+    it('it should split pids string', () => {
+      const pids = '2105,2106';
+      const pidsList = [
+        2105, 2106
+      ];
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.getPids(pids);
+      expect(component.pidsList).toEqual(pidsList);
+    })
+  })
+
+  describe('when getCores method is called', () => {
+    it('it should call getCoresDash', () => {
+      const cores = '23-31';
+      const coresList = [23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.getCores(cores);
+      expect(getCoresDashSpy).toHaveBeenCalledOnceWith(cores);
+      expect(component.coresList).toEqual(coresList);
+    })
+
+    it('it should split cores string', () => {
+      const cores = '41,42';
+      const coresList = [41, 42];
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.getCores(cores);
+      expect(component.coresList).toEqual(coresList);
+    })
+
+    it('it should throw error if cores exceeds MAX_CORES', () => {
+      const cores = '1025';
+      const coresList = [1025];
+      const coresError = 'limit cores to maximum number of 1024';
+
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      component.getCores(cores);
+      fixture.detectChanges();
+
+      const expectCoresError = ngMocks.formatText(
+        ngMocks.find(MatError)
+      );
+
+      expect(expectCoresError).toBe(coresError);
+      expect(component.coresList).toEqual(coresList);
+    })
+  })
+
+  describe('when user changes pool', () => {
+    const mockApp: Apps = {
+      id: 1,
+      name: 'app_2',
+      pids: [4156985, 4156986],
+      cores: [2],
+      pool_id: 0
+    }
+
+    it('it should display the correct cores for the current app', () => {
+      const poolId = mockApp.pool_id;
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      const event: MatSelectChange = {
+        source: {} as MatSelect,
+        value: poolId
+      }
+
+      component.poolChange(event);
+
+      expect(component.form.controls['cores'].value)
+        .toBe(mockApp.cores?.toString());
+    })
+
+    it('it should display the correct cores for that pool', () => {
+      const poolId = 1;
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      const event: MatSelectChange = {
+        source: {} as MatSelect,
+        value: poolId
+      }
+
+      component.poolChange(event);
+
+      expect(component.form.controls['cores'].value)
+        .toBe(mockedPools[poolId].cores.toString());
+    })
+  })
+
+  describe('when getPoolCores method is called', () => {
+    it('it should return the correct cores', () => {
+      const fixture = MockRender(AppsEditDialogComponent, {}, {
+        providers: [
+          {
+            provide: MAT_DIALOG_DATA,
+            useValue: { pools: mockedPools, app: mockApp }
+          }
+        ]
+      });
+      const component = fixture.point.componentInstance;
+
+      expect(component.getPoolCores(0)).toEqual(mockedPools[0].cores);
+      expect(component.getPoolCores(2)).toEqual(mockedPools[2].cores);
+    })
+  })
+})
