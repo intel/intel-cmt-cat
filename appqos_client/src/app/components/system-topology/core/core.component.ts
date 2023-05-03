42a97ecdacfca0e1e59524e68a29f6db895c2ebb
Marcel Cornu <marcel.d.cornu@intel.com>
2023-05-03 21:26:44 +0100

    appqos_client: implement core component

--- appqos_client/src/app/components/system-topology/core/core.component.ts
+++ appqos_client/src/app/components/system-topology/core/core.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CoreInfo } from '../../system-caps/system-caps.model';
+
+interface CoreColor {
+  bgColor: string;
+  fontColor: string;
+}
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.scss']
+  styleUrls: ['./core.component.scss'],
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent {
+  @Input() L2ID!: number;
+  @Input() cores!: CoreInfo[];
+  @Input() detailedView!: boolean;
+  // @todo extend colors to allow setting color for each pool
+  colors: CoreColor[] = [{ bgColor: '#50e1ff', fontColor: 'black' }];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
