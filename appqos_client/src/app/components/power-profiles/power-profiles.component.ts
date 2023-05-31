58b213c9e8feca9d8b605288d8f6125d52b31169
Shanahan, Corey <corey.shanahan@intel.com>
2023-05-31 13:17:37 +0100

    appqos_client: generated power profile component

---
+++ appqos_client/src/app/components/power-profiles/power-profiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-power-profiles',
+  templateUrl: './power-profiles.component.html',
+  styleUrls: ['./power-profiles.component.scss']
+})
+export class PowerProfilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
