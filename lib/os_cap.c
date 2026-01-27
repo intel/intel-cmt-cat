ffe6c1b4a22696c13e112db9e7405125bebada4e
Xiaochen Shen <shenxiaochen@open-hieco.net>
2026-01-27 15:25:26 +0800

    lib: Add L2 cache Non-Contiguous CBM capability detection with OS interface
    Currently, the capability detection of L2 cache Non-Contiguous CBM is
    missing with OS interface (--iface os). Regardless of whether the
    hardware supports it or not, the capability of L2 cache Non-Contiguous
    CBM always shows "unsupported":
    
    $ pqos --iface os -d | grep "Non-Contiguous CBM"
                    Non-Contiguous CBM: unsupported
    
    Add L2 cache Non-Contiguous CBM capability detection by reading
    'sparse_masks' file of L2 cache from kernel resctrl filesystem.
    
    After fix, if the hardware (e.g., some Intel platforms) supports:
    $ pqos --iface os -d | grep "Non-Contiguous CBM"
                    Non-Contiguous CBM: supported
    
    Fixes: 0a1dc0868e4a ("Non-Contiguous CBM support")
    Signed-off-by: Xiaochen Shen <shenxiaochen@open-hieco.net>

--- lib/os_cap.c
+++ lib/os_cap.c
@@ -539,6 +539,8 @@ os_cap_l2ca_discover(struct pqos_cap_l2ca *cap, const struct pqos_cpuinfo *cpu)
         const char *info;
         int cdp_on;
         int ret = PQOS_RETVAL_OK;
+        unsigned val;
+        char path[128];
 
         ASSERT(cap != NULL);
 
@@ -571,8 +573,17 @@ os_cap_l2ca_discover(struct pqos_cap_l2ca *cap, const struct pqos_cpuinfo *cpu)
 
         cap->way_size = cpu->l2.way_size * cpu->l2.num_ways / cap->num_ways;
 
+        snprintf(path, sizeof(path) - 1, "%s/sparse_masks", info);
+        ret = pqos_fread_uint(path, &val);
+        if (ret != PQOS_RETVAL_OK)
+                cap->non_contiguous_cbm = 0;
+        else
+                cap->non_contiguous_cbm = val;
+
         if (!cdp_on)
                 ret = pqos_file_contains(PROC_CPUINFO, "cdp_l2", &cap->cdp);
+        else
+                ret = PQOS_RETVAL_OK;
 
         return ret;
 }
