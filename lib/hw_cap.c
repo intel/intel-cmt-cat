intel/intel-cmt-cat

commit bfc7c706cb33866a5ed7d0578301a20ea1e557fa

SNC is added

lib/hw_cap.c
@@ -38,6 +38,7 @@
 
 #include "cpu_registers.h"
 #include "cpuinfo.h"
+#include "hw_monitoring.h"
 #include "log.h"
 #include "machine.h"
 #include "uncore_monitoring.h"
@@ -209,6 +210,85 @@ hw_cap_mon_iordt(const struct pqos_cpuinfo *cpu, int *enabled)
 }
 
 /**
+ * @brief Discovers HW monitoring SNC support
+ *
+ * @param [in] cpu detected CPU topology
+ * @param [out] snc_num Number of monitoring clusters
+ * @param [out] snc_mode SNC monitoring mode
+ *
+ * @return Operation status
+ * @retval PQOS_RETVAL_OK success
+ */
+PQOS_STATIC int
+hw_cap_mon_snc_state(const struct pqos_cpuinfo *cpu,
+                     unsigned *snc_num,
+                     enum pqos_snc_mode *snc_mode)
+{
+        int numa_num = cpuinfo_get_numa_num(cpu);
+        int socket_num = cpuinfo_get_socket_num(cpu);
+        unsigned i, sock_count, *sockets = NULL;
+        int ret = PQOS_RETVAL_OK;
+        enum pqos_snc_mode st;
+        enum pqos_snc_mode mode = (enum pqos_snc_mode)(-1);
+
+        if (cpu == NULL || snc_num == NULL || snc_mode == NULL)
+                return PQOS_RETVAL_PARAM;
+
+        if (numa_num < 0 || socket_num < 0)
+                return PQOS_RETVAL_ERROR;
+        if (numa_num == 0 || socket_num == 0 || numa_num == socket_num) {
+                *snc_num = 1;
+                *snc_mode = PQOS_SNC_LOCAL;
+                return PQOS_RETVAL_OK;
+        }
+
+        sockets = pqos_cpu_get_sockets(cpu, &sock_count);
+        if (sockets == NULL || sock_count == 0) {
+                printf("Error retrieving information for Sockets\n");
+                ret = PQOS_RETVAL_ERROR;
+                goto hw_cap_mon_snc_state_exit;
+        }
+
+        for (i = 0; i < sock_count; i++) {
+                unsigned lcore;
+                uint64_t val = 0;
+                uint32_t reg = PQOS_MSR_SNC_CFG;
+
+                ret = pqos_cpu_get_one_core(cpu, sockets[i], &lcore);
+                if (ret != PQOS_RETVAL_OK) {
+                        printf("Error retrieving lcore for socket %u\n",
+                               sockets[i]);
+                        ret = PQOS_RETVAL_ERROR;
+                        break;
+                };
+
+                if (msr_read(lcore, reg, &val) != MACHINE_RETVAL_OK) {
+                        ret = PQOS_RETVAL_ERROR;
+                        break;
+                }
+
+                st = (val & 1) ? PQOS_SNC_LOCAL : PQOS_SNC_TOTAL;
+                if (mode == (enum pqos_snc_mode)(-1))
+                        mode = st;
+                else if (mode != st) {
+                        printf("Error inconsistent SNC mode\n");
+                        ret = PQOS_RETVAL_ERROR;
+                        break;
+                };
+        };
+
+        if (ret == PQOS_RETVAL_OK) {
+                *snc_num = numa_num / socket_num;
+                *snc_mode = mode;
+        }
+
+hw_cap_mon_snc_state_exit:
+        if (sockets)
+                free(sockets);
+        return ret;
+}
+
+/**
  * @brief Discovers monitoring capabilities
  *
  * Runs series of CPUID instructions to discover system CMT
@@ -326,6 +406,14 @@ hw_cap_mon_discover(struct pqos_cap_mon **r_mon, const struct pqos_cpuinfo *cpu)
         mon->mem_size = sz;
         mon->max_rmid = max_rmid;
         mon->l3_size = l3_size;
+
+        /* Detect SNC state */
+        ret = hw_cap_mon_snc_state(cpu, &mon->snc_num, &mon->snc_mode);
+        if (ret != PQOS_RETVAL_OK) {
+                LOG_ERROR("Error reading SNC information!\n");
+                free(mon);
+                return PQOS_RETVAL_ERROR;
+        }
 
         {
                 const unsigned max_rmid = cpuid_0xf_1.ecx + 1;

lib/Makefile
@@ -97,11 +97,6 @@ ifeq ($(RMID_CUSTOM),y)
 CFLAGS += -DPQOS_RMID_CUSTOM
 endif
 
-# SNC
-ifeq ($(PQOS_SNC),y)
-CFLAGS += -DPQOS_SNC
-endif
-
 # Build targets and dependencies
 SRCS = $(sort $(wildcard *.c))
 OBJS = $(SRCS:.c=.o)

lib/api.c
@@ -781,6 +781,15 @@ pqos_mon_reset_config(const struct pqos_mon_config *cfg)
                                   cfg->l3_iordt);
                         return PQOS_RETVAL_PARAM;
                 }
+
+                if (cfg->snc != PQOS_REQUIRE_SNC_LOCAL &&
+                    cfg->snc != PQOS_REQUIRE_SNC_TOTAL &&
+                    cfg->snc != PQOS_REQUIRE_SNC_ANY) {
+                        LOG_ERROR("Unrecognized SNC Monitoring "
+                                  "configuration setting %d!\n",
+                                  cfg->snc);
+                        return PQOS_RETVAL_PARAM;
+                }
         }
 
         return API_CALL(mon_reset, cfg);

lib/cap.c
@@ -1140,6 +1140,36 @@ _pqos_cap_mon_iordt_change(const enum pqos_iordt_config iordt)
                 mon_cap->iordt_on = 0;
 }
 
+void
+_pqos_cap_mon_snc_change(const enum pqos_snc_config cfg)
+{
+        struct pqos_cap_mon *mon_cap = NULL;
+        unsigned i;
+
+        ASSERT(cfg == PQOS_REQUIRE_SNC_LOCAL || cfg == PQOS_REQUIRE_SNC_TOTAL ||
+               cfg == PQOS_REQUIRE_SNC_ANY);
+        ASSERT(m_sysconf.cap != NULL);
+
+        if (m_sysconf.cap == NULL)
+                return;
+
+        for (i = 0; i < m_sysconf.cap->num_cap && mon_cap == NULL; i++)
+                if (m_sysconf.cap->capabilities[i].type == PQOS_CAP_TYPE_MON)
+                        mon_cap = m_sysconf.cap->capabilities[i].u.mon;
+
+        if (mon_cap == NULL)
+                return;
+
+        if (mon_cap->snc_num == 1)
+                return;
+
+        if (cfg == PQOS_REQUIRE_SNC_TOTAL)
+                mon_cap->snc_mode = PQOS_SNC_TOTAL;
+
+        if (cfg == PQOS_REQUIRE_SNC_LOCAL)
+                mon_cap->snc_mode = PQOS_SNC_LOCAL;
+}
+
 const struct pqos_sysconfig *
 _pqos_get_sysconfig(void)
 {

lib/cap.h
@@ -96,6 +96,16 @@ PQOS_LOCAL void _pqos_cap_mba_change(const enum pqos_mba_config cfg);
 void _pqos_cap_mon_iordt_change(const enum pqos_iordt_config iordt);
 
 /**
+ * @brief Modifies Mon SNC capability structure upon SNC config change
+ *
+ * Limited error checks done in this function and no errors reported.
+ * It is up to caller to check for MON & SNC support.
+ *
+ * @param [in] snc SNC configuration
+ */
+PQOS_LOCAL void _pqos_cap_mon_snc_change(const enum pqos_snc_config cfg);
+
+/**
  * @brief Checks library initialization state
  *
  * @param expect expected stated of library initialization state

lib/cpu_registers.h
@@ -80,6 +80,7 @@ extern "C" {
 #define PQOS_MSR_L2_QOS_CFG_CDP_EN 1ULL  /**< L2 CDP enable bit */
 
 #define PQOS_MSR_MBA_CFG 0xC84 /**< MBA config register */
+#define PQOS_MSR_SNC_CFG 0xCA0 /**< SNC config register */
 /**
  * MBA linear max value
  */

lib/cpuinfo.c
@@ -44,6 +44,7 @@
 #include "machine.h"
 #include "os_allocation.h"
 #include "os_cpuinfo.h"
+#include "utils.h"
 
 #include <errno.h>
 #include <limits.h>
@@ -399,25 +400,18 @@ detect_cpu(const int cpu,
         info->l3_id = apicid >> apic->l3_shift;
         info->l2_id = apicid >> apic->l2_shift;
 
-#if (PQOS_VERSION >= 50000 || defined PQOS_SNC)
 #if defined __linux__ && defined __NR_getcpu
-        syscall(__NR_getcpu, NULL, &info->numa, NULL);
+        if (syscall(__NR_getcpu, NULL, &info->numa, NULL) != 0)
+                return -1;
 #else
         if (getcpu(NULL, &info->numa) != 0)
                 return -1;
 #endif
-#endif
 
-#if (PQOS_VERSION >= 50000 || defined PQOS_SNC)
         LOG_DEBUG("Detected core %u, socket %u, NUMAnode %u, "
                   "L2 ID %u, L3 ID %u, APICID %u\n",
                   info->lcore, info->socket, info->numa, info->l2_id,
                   info->l3_id, apicid);
-#else
-        LOG_DEBUG("Detected core %u, socket %u, "
-                  "L2 ID %u, L3 ID %u, APICID %u\n",
-                  info->lcore, info->socket, info->l2_id, info->l3_id, apicid);
-#endif
 
         return 0;
 }
@@ -677,4 +671,42 @@ cpuinfo_get_config(const struct cpuinfo_config **config)
         ASSERT(config != NULL);
 
         *config = &m_config;
+}
+
+int
+cpuinfo_get_numa_num(const struct pqos_cpuinfo *cpu)
+{
+        unsigned *numa;
+        unsigned count;
+
+        /* Not all CPUs are online fallback to read from OS */
+        if (sysconf(_SC_NPROCESSORS_CONF) != sysconf(_SC_NPROCESSORS_ONLN))
+                return os_cpuinfo_get_numa_num();
+
+        numa = pqos_cpu_get_numa(cpu, &count);
+        if (numa != NULL) {
+                free(numa);
+                return count;
+        }
+
+        return -1;
+}
+
+int
+cpuinfo_get_socket_num(const struct pqos_cpuinfo *cpu)
+{
+        unsigned *socket;
+        unsigned count;
+
+        /* Not all CPUs are online fallback to read from OS */
+        if (sysconf(_SC_NPROCESSORS_CONF) != sysconf(_SC_NPROCESSORS_ONLN))
+                return os_cpuinfo_get_socket_num();
+
+        socket = pqos_cpu_get_sockets(cpu, &count);
+        if (socket != NULL) {
+                free(socket);
+                return count;
+        }
+
+        return -1;
 }

lib/cpuinfo.h
@@ -95,6 +95,26 @@ PQOS_LOCAL int cpuinfo_fini(void);
 PQOS_LOCAL void cpuinfo_get_config(const struct cpuinfo_config **config);
 
 /**
+ * @brief Helper function to get number of numa nodes in the system
+ *
+ * @param [in] cpu CPU topology structure
+ *
+ * @return Number of numa nodes in the system
+ * @retval 0 if not successful
+ */
+PQOS_LOCAL int cpuinfo_get_numa_num(const struct pqos_cpuinfo *cpu);
+
+/**
+ * @brief Helper function to get number of sockets in the system
+ *
+ * @param [in] cpu CPU topology structure
+ *
+ * @return Number of sockets in the system
+ * @retval 0 if not successful
+ */
+PQOS_LOCAL int cpuinfo_get_socket_num(const struct pqos_cpuinfo *cpu);
+
+/**
  * @brief Detect cpu model
  *
  * @return detected cpu model

commit 5afb3247800ccf3fcc18ffa0c52d421312bad8ef

lib: Fix SNC state detection on non-SNC-capable platforms
On platforms that do not support Sub-NUMA Clustering (SNC) monitoring
(e.g., AMD and Hygon), if the CPU topology indicates that the number of
NUMA nodes and the number of sockets are not equal, the SNC state
detection logic in hw_cap_mon_snc_state() will attempt to read the SNC
configuration MSR register PQOS_MSR_SNC_CFG (0xCA0) that is not
available. This causes the command "pqos --iface=msr" to fail with the
error:

  "ERROR: RDMSR failed for reg[0xca0] on lcore 0"
  "ERROR: Error reading SNC information!"
  "ERROR: Error encounter in monitoring discovery!"
  "ERROR: discover_capabilities() error 1"
  "Error initializing PQoS library!"

Fix the issue by ensuring hw_cap_mon_snc_state() returns early on
non-SNC-capable platforms.

Fixes: bfc7c706cb33 ("SNC is added")
Signed-off-by: Xiaochen Shen <shenxiaochen@open-hieco.net>

lib/hw_cap.c
@@ -237,6 +237,19 @@ hw_cap_mon_snc_state(const struct pqos_cpuinfo *cpu,
 
         if (numa_num < 0 || socket_num < 0)
                 return PQOS_RETVAL_ERROR;
+
+        /**
+         * On platforms that do not support Sub-NUMA Clustering (SNC)
+         * monitoring (e.g., AMD and Hygon platforms).
+         *
+         * Set snc_num = 1 and return early.
+         */
+        if (cpu->vendor == PQOS_VENDOR_AMD ||
+            cpu->vendor == PQOS_VENDOR_HYGON) {
+                *snc_num = 1;
+                return PQOS_RETVAL_OK;
+        }
+
         if (numa_num == 0 || socket_num == 0 || numa_num == socket_num) {
                 *snc_num = 1;
                 *snc_mode = PQOS_SNC_LOCAL;