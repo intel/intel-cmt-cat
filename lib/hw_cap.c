intel/intel-cmt-cat

commit 0a1dc0868e4a6de8ec9214797769fb1723cec52b

Non-Contiguous CBM support

lib/hw_cap.c
@@ -520,6 +520,8 @@ hw_cap_l3ca_cpuid(struct pqos_cap_l3ca *cap, const struct pqos_cpuinfo *cpu)
         cap->cdp = (res.ecx >> PQOS_CPUID_CAT_CDP_BIT) & 1;
         cap->cdp_on = 0;
         cap->way_contention = (uint64_t)res.ebx;
+        cap->non_contiguous_cbm =
+            (res.ecx >> PQOS_CPUID_CAT_NON_CONTIGUOUS_CBM_SUPPORT_BIT) & 1;
 
         if (cap->cdp) {
                 /**
@@ -713,6 +715,8 @@ hw_cap_l2ca_discover(struct pqos_cap_l2ca *cap, const struct pqos_cpuinfo *cpu)
         cap->cdp = (res.ecx >> PQOS_CPUID_CAT_CDP_BIT) & 1;
         cap->cdp_on = 0;
         cap->way_contention = (uint64_t)res.ebx;
+        cap->non_contiguous_cbm =
+            (res.ecx >> PQOS_CPUID_CAT_NON_CONTIGUOUS_CBM_SUPPORT_BIT) & 1;
 
         if (cap->cdp) {
                 int cdp_on = 0;

appqos/appqos/caps.py
@@ -116,6 +116,17 @@ def cdp_l3_supported(iface):
     """
     return common.CDP_L3_CAP in caps_get(iface)
 
+def is_l3_non_contiguous_cbm_supported(iface):
+    """
+    Returns L3 CDP Non-contiguous CBM support status
+    """
+    return common.NON_CONTIGUOUS_CBM_L3_CAP in caps_get(iface)
+
+def is_l2_non_contiguous_cbm_supported(iface):
+    """
+    Returns L2 Non-contiguous CBM support status
+    """
+    return common.NON_CONTIGUOUS_CBM_L2_CAP in caps_get(iface)
 
 def cat_l2_supported(iface):
     """
@@ -181,12 +192,16 @@ def detect_supported_caps():
         result.append(common.CAT_L3_CAP)
         if PQOS_API.is_l3_cdp_supported():
             result.append(common.CDP_L3_CAP)
+        if PQOS_API.get_l3_non_contiguous_cbm():
+            result.append(common.NON_CONTIGUOUS_CBM_L3_CAP)
 
     # Intel RDT L2 CAT
     if PQOS_API.is_l2_cat_supported():
         result.append(common.CAT_L2_CAP)
         if PQOS_API.is_l2_cdp_supported():
             result.append(common.CDP_L2_CAP)
+        if PQOS_API.get_l2_non_contiguous_cbm():
+            result.append(common.NON_CONTIGUOUS_CBM_L2_CAP)
 
     # Intel RDT MBA
     if PQOS_API.is_mba_supported():
@@ -258,7 +273,8 @@ def l3ca_info():
         'cache_ways_num': rdt_api.get_l3_num_cache_ways(),
         'clos_num': rdt_api.get_l3ca_num_cos(),
         'cdp_supported': rdt_api.is_l3_cdp_supported(),
-        'cdp_enabled': rdt_api.is_l3_cdp_enabled()
+        'cdp_enabled': rdt_api.is_l3_cdp_enabled(),
+        'non_contiguous_cbm': rdt_api.get_l3_non_contiguous_cbm()
     }
     return info
 
@@ -284,6 +300,7 @@ def l2ca_info():
         'cache_ways_num': rdt_api.get_l2_num_cache_ways(),
         'clos_num': rdt_api.get_l2ca_num_cos(),
         'cdp_supported': rdt_api.is_l2_cdp_supported(),
-        'cdp_enabled': rdt_api.is_l2_cdp_enabled()
+        'cdp_enabled': rdt_api.is_l2_cdp_enabled(),
+        'non_contiguous_cbm': rdt_api.get_l2_non_contiguous_cbm()
     }
     return info

appqos/appqos/common.py
@@ -43,6 +43,8 @@ CAT_L3_CAP = "l3cat"
 CAT_L2_CAP = "l2cat"
 CDP_L3_CAP = "l3cdp"
 CDP_L2_CAP = "l2cdp"
+NON_CONTIGUOUS_CBM_L3_CAP = "l3_non_contiguous_cbm"
+NON_CONTIGUOUS_CBM_L2_CAP = "l2_non_contiguous_cbm"
 DEFAULT_ADDRESS = "127.0.0.1"
 MBA_CAP = "mba"
 SSTBF_CAP = "sstbf"

appqos/appqos/config_store.py
@@ -220,6 +220,7 @@ class ConfigStore:
         if l3cdp_enabled and not caps.cdp_l3_supported(iface):
             raise ValueError("RDT Configuration. L3 CDP requested but not supported!")
 
+        non_contiguous_cbm = caps.is_l3_non_contiguous_cbm_supported(iface)
         cdp_pool_ids = []
 
         for pool in data['pools']:
@@ -227,10 +228,15 @@ class ConfigStore:
                 if not cbm in pool:
                     continue
 
-                result = re.search('1{1,32}0{1,32}1{1,32}', bin(pool[cbm]))
-                if result or pool[cbm] == 0:
-                    raise ValueError(f"Pool {pool['id']}, " \
-                        f"L3 CBM {hex(pool['l3cbm'])}/{bin(pool[cbm])} is not contiguous.")
+                if pool[cbm] == 0:
+                    raise ValueError(f"Pool {pool['id']}, L3 CBM is zero.")
+
+                if not non_contiguous_cbm:
+                    result = re.search('1{1,32}0{1,32}1{1,32}', bin(pool[cbm]))
+                    if result:
+                        raise ValueError(f"Pool {pool['id']}, " \
+                            f"L3 CBM {hex(pool['l3cbm'])}/{bin(pool[cbm])} is not contiguous.")
+
                 if not caps.cat_l3_supported(iface):
                     raise ValueError(f"Pool {pool['id']}, " \
                         f"L3 CBM {hex(pool['l3cbm'])}/{bin(pool[cbm])}, L3 CAT is not supported.")
@@ -257,6 +263,7 @@ class ConfigStore:
         if l2cdp_enabled and not caps.cdp_l2_supported(iface):
             raise ValueError("RDT Configuration. L2 CDP requested but not supported!")
 
+        non_contiguous_cbm = caps.is_l2_non_contiguous_cbm_supported(iface)
         cdp_pool_ids = []
 
         for pool in data['pools']:
@@ -264,10 +271,15 @@ class ConfigStore:
                 if not cbm in pool:
                     continue
 
-                result = re.search('1{1,32}0{1,32}1{1,32}', bin(pool[cbm]))
-                if result or pool[cbm] == 0:
-                    raise ValueError(f"Pool {pool['id']}, " \
-                        f"L2 CBM {hex(pool['l2cbm'])}/{bin(pool[cbm])} is not contiguous.")
+                if pool[cbm] == 0:
+                    raise ValueError(f"Pool {pool['id']}, L2 CBM is zero.")
+
+                if not non_contiguous_cbm:
+                    result = re.search('1{1,32}0{1,32}1{1,32}', bin(pool[cbm]))
+                    if result:
+                        raise ValueError(f"Pool {pool['id']}, " \
+                            f"L2 CBM {hex(pool['l2cbm'])}/{bin(pool[cbm])} is not contiguous.")
+
                 if not caps.cat_l2_supported(iface):
                     raise ValueError(f"Pool {pool['id']}, " \
                         f"L2 CBM {hex(pool['l2cbm'])}/{bin(pool[cbm])}, L2 CAT is not supported.")

appqos/appqos/pqos_api.py
@@ -746,6 +746,26 @@ class PqosApi:
             log.error(str(ex))
             return None
 
+    def get_l3_non_contiguous_cbm(self):
+        # pylint: disable=no-self-use
+        """
+        Gets L3 cache non-contiguous cbm
+
+        Returns:
+            L3 cache non-contiguous cbm
+            or None on error
+        """
+
+        if not self.is_l3_cat_supported():
+            return None
+
+        try:
+            l3ca_caps = self.cap.get_type("l3ca")
+            return l3ca_caps.non_contiguous_cbm
+        except Exception as ex:
+            log.error(str(ex))
+            return None
+
     def get_l2_cache_size(self):
         # pylint: disable=no-self-use
         """
@@ -844,6 +864,26 @@ class PqosApi:
         try:
             l2ca_caps = self.cap.get_type("l2ca")
             return l2ca_caps.cdp_on
+        except Exception as ex:
+            log.error(str(ex))
+            return None
+
+    def get_l2_non_contiguous_cbm(self):
+        # pylint: disable=no-self-use
+        """
+        Gets L2 cache non-contiguous cbm
+
+        Returns:
+            L2 cache non-contiguous cbm
+            or None on error
+        """
+
+        if not self.is_l2_cat_supported():
+            return None
+
+        try:
+            l2ca_caps = self.cap.get_type("l2ca")
+            return l2ca_caps.non_contiguous_cbm
         except Exception as ex:
             log.error(str(ex))
             return None

lib/allocation.c
@@ -291,6 +291,20 @@ hw_l3ca_set(const unsigned l3cat_id,
         ASSERT(ca != NULL);
         ASSERT(num_ca != 0);
 
+        /* Check L3 CBM is non-contiguous */
+        if (!cap_get_l3ca_non_contignous()) {
+                unsigned idx;
+                /* Check all COS CBM are contiguous */
+                for (idx = 0; idx < num_ca; idx++) {
+                        if (!IS_CONTIGNOUS(ca[idx])) {
+                                LOG_ERROR("L3 CAT COS%u bit mask is not "
+                                          "contiguous!\n",
+                                          ca[idx].class_id);
+                                return PQOS_RETVAL_PARAM;
+                        }
+                }
+        }
+
         ret = pqos_l3ca_get_cos_num(cap, &count);
         if (ret != PQOS_RETVAL_OK)
                 return ret; /**< perhaps no L3CA capability */
@@ -557,6 +571,20 @@ hw_l2ca_set(const unsigned l2id,
 
         ASSERT(ca != NULL);
         ASSERT(num_ca != 0);
+
+        /* Check L2 CBM is non-contiguous */
+        if (!cap_get_l2ca_non_contignous()) {
+                unsigned idx;
+                /* Check all COS CBM are contiguous */
+                for (idx = 0; idx < num_ca; idx++) {
+                        if (!IS_CONTIGNOUS(ca[idx])) {
+                                LOG_ERROR("L2 CAT COS%u bit mask is not "
+                                          "contiguous!\n",
+                                          ca[idx].class_id);
+                                return PQOS_RETVAL_PARAM;
+                        }
+                }
+        }
 
         /*
          * Check if L2 CAT is supported
@@ -1644,4 +1672,19 @@ pqos_alloc_reset_exit:
         if (l2ids != NULL)
                 free(l2ids);
         return ret;
+}
+
+int
+alloc_is_bitmask_contiguous(uint64_t bitmask)
+{
+        if (bitmask == 0)
+                return 0;
+
+        while ((bitmask & 1) == 0) /**< Shift until 1 found at position 0 */
+                bitmask >>= 1;
+
+        while ((bitmask & 1) != 0) /**< Shift until 0 found at position 0 */
+                bitmask >>= 1;
+
+        return (bitmask) ? 0 : 1; /**< non-zero bitmask is not contiguous */
 }

lib/allocation.h
@@ -45,6 +45,19 @@ extern "C" {
 #include "pqos.h"
 #include "types.h"
 
+#define IS_CONTIGNOUS(ca)                                                      \
+        ({                                                                     \
+                int ret;                                                       \
+                                                                               \
+                if (ca.cdp)                                                    \
+                        ret = alloc_is_bitmask_contiguous(ca.u.s.data_mask) && \
+                              alloc_is_bitmask_contiguous(ca.u.s.code_mask);   \
+                else                                                           \
+                        ret = alloc_is_bitmask_contiguous(ca.u.ways_mask);     \
+                                                                               \
+                ret;                                                           \
+        })
+
 /**
  * Types of possible PQoS allocation technologies
  */
@@ -421,6 +434,22 @@ PQOS_LOCAL int hw_mba_get_amd(const unsigned mba_id,
                               const unsigned max_num_cos,
                               unsigned *num_cos,
                               struct pqos_mba *mba_tab);
+
+/**
+ * @brief Tests if \a bitmask is contiguous
+ *
+ * Zero bit mask is regarded as not contiguous.
+ *
+ * The function shifts out first group of contiguous 1's in the bit mask.
+ * Next it checks remaining bitmask content to make a decision.
+ *
+ * @param bitmask bit mask to be validated for contiguity
+ *
+ * @return Bit mask contiguity check result
+ * @retval 0 not contiguous
+ * @retval 1 contiguous
+ */
+PQOS_LOCAL int alloc_is_bitmask_contiguous(uint64_t bitmask);
 
 #ifdef __cplusplus
 }

lib/api.c
@@ -431,35 +431,6 @@ pqos_pid_get_pid_assoc(const unsigned class_id, unsigned *count)
  * =======================================
  */
 
-/**
- * @brief Tests if \a bitmask is contiguous
- *
- * Zero bit mask is regarded as not contiguous.
- *
- * The function shifts out first group of contiguous 1's in the bit mask.
- * Next it checks remaining bitmask content to make a decision.
- *
- * @param bitmask bit mask to be validated for contiguity
- *
- * @return Bit mask contiguity check result
- * @retval 0 not contiguous
- * @retval 1 contiguous
- */
-static int
-is_contiguous(uint64_t bitmask)
-{
-        if (bitmask == 0)
-                return 0;
-
-        while ((bitmask & 1) == 0) /**< Shift until 1 found at position 0 */
-                bitmask >>= 1;
-
-        while ((bitmask & 1) != 0) /**< Shift until 0 found at position 0 */
-                bitmask >>= 1;
-
-        return (bitmask) ? 0 : 1; /**< non-zero bitmask is not contiguous */
-}
-
 int
 pqos_l3ca_set(const unsigned l3cat_id,
               const unsigned num_cos,
@@ -471,20 +442,19 @@ pqos_l3ca_set(const unsigned l3cat_id,
                 return PQOS_RETVAL_PARAM;
 
         /**
-         * Check if class bitmasks are contiguous.
+         * Check if class bitmasks are zero.
          */
         for (i = 0; i < num_cos; i++) {
-                int is_contig = 0;
+                int is_non_zero = 0;
 
-                if (ca[i].cdp) {
-                        is_contig = is_contiguous(ca[i].u.s.data_mask) &&
-                                    is_contiguous(ca[i].u.s.code_mask);
-                } else
-                        is_contig = is_contiguous(ca[i].u.ways_mask);
+                if (ca[i].cdp)
+                        is_non_zero =
+                            ca[i].u.s.data_mask && ca[i].u.s.code_mask;
+                else
+                        is_non_zero = ca[i].u.ways_mask;
 
-                if (!is_contig) {
-                        LOG_ERROR("L3 COS%u bit mask is not contiguous!\n",
-                                  ca[i].class_id);
+                if (!is_non_zero) {
+                        LOG_ERROR("L3 COS%u bit mask is 0!\n", ca[i].class_id);
                         return PQOS_RETVAL_PARAM;
                 }
         }
@@ -530,20 +500,19 @@ pqos_l2ca_set(const unsigned l2id,
                 return PQOS_RETVAL_PARAM;
 
         /**
-         * Check if class bitmasks are contiguous
+         * Check if class bitmasks are zero.
          */
         for (i = 0; i < num_cos; i++) {
-                int is_contig = 0;
+                int is_non_zero = 0;
 
-                if (ca[i].cdp) {
-                        is_contig = is_contiguous(ca[i].u.s.data_mask) &&
-                                    is_contiguous(ca[i].u.s.code_mask);
-                } else
-                        is_contig = is_contiguous(ca[i].u.ways_mask);
+                if (ca[i].cdp)
+                        is_non_zero =
+                            ca[i].u.s.data_mask && ca[i].u.s.code_mask;
+                else
+                        is_non_zero = ca[i].u.ways_mask;
 
-                if (!is_contig) {
-                        LOG_ERROR("L2 COS%u bit mask is not contiguous!\n",
-                                  ca[i].class_id);
+                if (!is_non_zero) {
+                        LOG_ERROR("L2 COS%u bit mask is 0!\n", ca[i].class_id);
                         return PQOS_RETVAL_PARAM;
                 }
         }

commit ee6128e02c631ee7ee5508065ec3b4d42f82b36b

lib: Fix L3 cache Non-Contiguous CBM capability with MSR interface for AMD and Hygon
From Intel RDT spec[1] and AMD Platform QoS spec[2]:
The Non-Contiguous CBM support bit (CPUID.0x10.1:ECX[3]) is Intel
platform specific.

AMD and Hygon platforms support L3 cache Non-Contiguous CBM, but the
Non-Contiguous CBM support bit (CPUID.0x10.1:ECX[3]) is unused.

Currently, the capability of L3 cache Non-Contiguous CBM is incorrectly
initialized with MSR interface (--iface msr) on AMD and Hygon platforms:

$ pqos --iface msr -d | grep "Non-Contiguous CBM"
                Non-Contiguous CBM: unsupported

Fix it by setting L3 cache Non-Contiguous CBM supported for AMD and
Hygon platforms.

After fix:
$ pqos --iface msr -d | grep "Non-Contiguous CBM"
                Non-Contiguous CBM: supported

[1] Intel Architectures SDM, Vol.3B, 19.19 Intel RDT Allocation:
https://cdrdv2.intel.com/v1/dl/getContent/671200

[2] AMD Platform QoS Extensions, Rev 1.03:
https://www.amd.com/content/dam/amd/en/documents/processor-tech-docs/other/56375_1_03_PUB.pdf

Fixes: 0a1dc0868e4a ("Non-Contiguous CBM support")
Signed-off-by: Xiaochen Shen <shenxiaochen@open-hieco.net>

lib/hw_cap.c
@@ -764,8 +764,27 @@ hw_cap_l3ca_cpuid(struct pqos_cap_l3ca *cap, const struct pqos_cpuinfo *cpu)
         cap->cdp = (res.ecx >> PQOS_CPUID_CAT_CDP_BIT) & 1;
         cap->cdp_on = 0;
         cap->way_contention = (uint64_t)res.ebx;
-        cap->non_contiguous_cbm =
-            (res.ecx >> PQOS_CPUID_CAT_NON_CONTIGUOUS_CBM_SUPPORT_BIT) & 1;
+
+        switch (cpu->vendor) {
+        case PQOS_VENDOR_INTEL:
+                cap->non_contiguous_cbm =
+                    (res.ecx >> PQOS_CPUID_CAT_NON_CONTIGUOUS_CBM_SUPPORT_BIT) &
+                    1;
+                break;
+        case PQOS_VENDOR_AMD:
+        case PQOS_VENDOR_HYGON:
+                /**
+                 * AMD and Hygon platforms support L3 cache Non-Contiguous CBM,
+                 * but the Non-Contiguous CBM support bit (CPUID.0x10.1:ECX[3])
+                 * is unused.
+                 */
+                cap->non_contiguous_cbm = 1;
+                break;
+        default:
+                cap->non_contiguous_cbm = 0;
+                break;
+        }
+
         cap->iordt = (res.ecx >> PQOS_CPUID_CAT_IORDT_BIT) & 1;
         cap->iordt_on = 0;
 